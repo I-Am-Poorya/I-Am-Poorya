41bc405d1f2415921c71e8e5f2be3eb53129efa6
I-AM-Poorya <p.aliabadia81@gmail.com>
2024-04-29 20:28:05 -0700

    User json

---
+++ source/js/script.js
@@ -0,0 +1,26 @@
+let userJson = `[
+    {
+        "id": 1 , 
+        "username" : "amin" , 
+        "password" : "hi1"
+    }
+    ,
+    {
+        "id": 2 , 
+        "username" : "amir" , 
+        "password" : "hi2"
+    }
+    ,
+    {
+        "id": 3 , 
+        "username" : "poorya" , 
+        "password" : "hi3"
+    }
+    ,
+    {
+        "id": 4 , 
+        "username" : "parsa" , 
+        "password" : "hi4"
+    }
+]`
+console.log(JSON.parse(userJson))
